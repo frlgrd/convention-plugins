f4f3995c158f2e5f33933c28623a669652ad5a6e
François Legrand <francois.legrand@blablacar.com>
2025-08-04 01:01:15 +0200

    add line breaks

--- build-logic/conventions/src/main/kotlin/extensions/DependencyHandlerScopeExtensions.kt
+++ build-logic/conventions/src/main/kotlin/extensions/DependencyHandlerScopeExtensions.kt
@@ -3,7 +3,11 @@ package extensions
 import org.gradle.kotlin.dsl.DependencyHandlerScope
 
 fun DependencyHandlerScope.ksp(id: Any) = "ksp"(id)
+
 fun DependencyHandlerScope.implementation(id: Any) = "implementation"(id)
+
 fun DependencyHandlerScope.testImplementation(id: Any) = "testImplementation"(id)
+
 fun DependencyHandlerScope.debugImplementation(id: Any) = "debugImplementation"(id)
+
 fun DependencyHandlerScope.androidTestImplementation(id: Any) = "androidTestImplementation"(id)
